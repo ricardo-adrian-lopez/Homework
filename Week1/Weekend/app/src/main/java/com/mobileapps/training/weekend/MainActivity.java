8910dfd9366e44c8c18b54133f137038fe4f2949
Ricardo Lopez <ricardo.lopez@mobileappscompany.us>
2018-07-04 21:10:37 -0400

    Daily 2 Organization Chart

--- Week1/Weekend/app/src/main/java/com/mobileapps/training/weekend/MainActivity.java
+++ Week1/Weekend/app/src/main/java/com/mobileapps/training/weekend/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
@@ -41,6 +42,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
     }
 
     @Override
