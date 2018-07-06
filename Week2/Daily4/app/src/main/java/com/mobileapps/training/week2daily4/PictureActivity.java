37f3e021592c50ee8af8b8a70932b019f10a840f
Ricardo Lopez <ricardo.lopez@mobileappscompany.us>
2018-07-05 23:25:04 -0400

    Added the functionality to take a picture with the camera and place it in an imageView

--- Week2/Daily4/app/src/main/java/com/mobileapps/training/week2daily4/PictureActivity.java
+++ Week2/Daily4/app/src/main/java/com/mobileapps/training/week2daily4/PictureActivity.java
@@ -1,7 +1,47 @@
 package com.mobileapps.training.week2daily4;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
 public class PictureActivity extends BaseActivity {
 
+    Button buttonPicture;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    ImageView imageView;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picture);
 
+        buttonPicture = findViewById(R.id.btnPicture);
+        imageView =  findViewById(R.id.imgViewPicture);
+        buttonPicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
 }
