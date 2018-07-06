5d9df175ef2851c4efc1a20d250e20367aed9833
Ricardo Lopez <ricardo.lopez@mobileappscompany.us>
2018-07-05 22:51:04 -0400

    Added the missing icons and fixed the broken toolbar functionality

--- Week2/Daily4/app/src/main/java/com/mobileapps/training/week2daily4/BaseActivity.java
+++ Week2/Daily4/app/src/main/java/com/mobileapps/training/week2daily4/BaseActivity.java
@@ -5,20 +5,22 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.FrameLayout;
 
 public class BaseActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private static final String TAG = "BaseActivity";
 
-    DrawerLayout fullLayout;
-    Toolbar toolbar;
-    NavigationView navigationView;
+    private NavigationView navigationView;
+    private DrawerLayout fullLayout;
+    private Toolbar toolbar;
+    private ActionBarDrawerToggle drawerToggle;
+    private int selectedNavItemId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,20 +30,74 @@ public class BaseActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public void setContentView(int layoutResID) {
+        /**
+         * This is going to be our actual root layout.
+         */
         fullLayout = (DrawerLayout) getLayoutInflater().inflate(R.layout.activity_base, null);
-        FrameLayout activityContainer = fullLayout.findViewById(R.id.activity_content);
+        /**
+         * {@link FrameLayout} to inflate the child's view. We could also use a {@link android.view.ViewStub}
+         */
+        FrameLayout activityContainer = (FrameLayout) fullLayout.findViewById(R.id.activity_content);
         getLayoutInflater().inflate(layoutResID, activityContainer, true);
 
-        toolbar = findViewById(R.id.toolbar);
-        Log.d(TAG, "setContentView: Toolbar: " + toolbar);
-        setSupportActionBar(toolbar);
-        setTitle("Week2 Daily2");
-        ActionBar actionbar = getSupportActionBar();
-        actionbar.setHomeAsUpIndicator(R.drawable.ic_home_black_24dp);
-        actionbar.setDisplayHomeAsUpEnabled(true);
+        /**
+         * Note that we don't pass the child's layoutId to the parent,
+         * instead we pass it our inflated layout.
+         */
+        super.setContentView(fullLayout);
 
-        navigationView = findViewById(R.id.nav_view);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        navigationView = (NavigationView) findViewById(R.id.navigationView);
+
+        if (useToolbar())
+        {
+            setSupportActionBar(toolbar);
+        }
+        else
+        {
+            toolbar.setVisibility(View.GONE);
+        }
+
+        setUpNavView();
+    }
+
+    /**
+     * Helper method that can be used by child classes to
+     * specify that they don't want a {@link Toolbar}
+     * @return true
+     */
+    protected boolean useToolbar()
+    {
+        return true;
+    }
+
+    protected void setUpNavView()
+    {
         navigationView.setNavigationItemSelectedListener(this);
+
+        if( useDrawerToggle()) { // use the hamburger menu
+            drawerToggle = new ActionBarDrawerToggle(this, fullLayout, toolbar,
+                    R.string.nav_drawer_opened,
+                    R.string.nav_drawer_closed);
+
+            fullLayout.setDrawerListener(drawerToggle);
+            drawerToggle.syncState();
+        } else if(useToolbar() && getSupportActionBar() != null) {
+            // Use home/back button instead
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeAsUpIndicator(getResources()
+                    .getDrawable(R.drawable.ic_arrow_back_black_24dp));
+        }
+    }
+
+    /**
+     * Helper method to allow child classes to opt-out of having the
+     * hamburger menu.
+     * @return
+     */
+    protected boolean useDrawerToggle()
+    {
+        return true;
     }
 
     @Override
@@ -57,11 +113,11 @@ public class BaseActivity extends AppCompatActivity implements NavigationView.On
             case R.id.nav_home:
                 Intent intentHome = new Intent(this, MainActivity.class);
                 startActivity(intentHome);
+                return true;
             case R.id.nav_camera:
                 Intent intentPicture = new Intent(this, PictureActivity.class);
                 startActivity(intentPicture);
-            case R.id.nav_share_string:
-
+                return true;
         }
         return super.onOptionsItemSelected(item);
     }
