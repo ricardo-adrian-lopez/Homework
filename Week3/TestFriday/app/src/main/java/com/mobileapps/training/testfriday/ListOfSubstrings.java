7b939a9ebc2eb014e659f69f8d8b36eb06c7201d
Ricardo Lopez <ricardo.lopez@mobileappscompany.us>
2018-07-13 20:45:42 -0400

    List of substrings added

---
+++ Week3/TestFriday/app/src/main/java/com/mobileapps/training/testfriday/ListOfSubstrings.java
@@ -0,0 +1,28 @@
+package com.mobileapps.training.testfriday;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ListOfSubstrings {
+
+    public static void main(String[] args) {
+        String string = "abcd";
+        List<String> list = collectSubstrings(string);
+        System.out.println(list);
+    }
+
+    private static List<String> collectSubstrings(String string) throws ArrayIndexOutOfBoundsException{
+        List<String> collect = new ArrayList<>();
+        int skip;
+            for (int i = 0; i <string.length() ; i++) {
+                    skip=string.length()-i;
+                while (skip > 0) {
+                    String val = string.substring(i,(string.length()-skip)+1);
+                    collect.add(val);
+                    skip--;
+                }
+            }
+        return collect;
+    }
+}
