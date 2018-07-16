a8aa229fa3ed0a560df85779d02bed310c3ad0b6
Ricardo Lopez <ricardo.lopez@mobileappscompany.us>
2018-07-15 23:14:46 -0400

    Add Service with Notification

--- Week3/Daily4/app/src/main/java/com/mobileapps/training/week3daily4/MainActivity.java
+++ Week3/Daily4/app/src/main/java/com/mobileapps/training/week3daily4/MainActivity.java
@@ -1,13 +1,43 @@
 package com.mobileapps.training.week3daily4;
 
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnStart;
+    Button btnStop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnStart = findViewById(R.id.btnStart);
+        btnStop = findViewById(R.id.btnStop);
+
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentStart = new Intent(MainActivity.this, SoundService.class);
+                //startForeground(1,notification);
+                startService(intentStart);
+            }
+        });
+
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentStop = new Intent(MainActivity.this, SoundService.class);
+                stopService(intentStop);
+            }
+        });
     }
 }
