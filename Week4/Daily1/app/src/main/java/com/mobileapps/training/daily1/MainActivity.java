62ebf2204e2f57698bc1aef880bf8069748b82ec
Ricardo Lopez <ricardo.lopez@mobileappscompany.us>
2018-07-16 20:42:16 -0400

    Added support for 5 system broadcast

--- Week4/Daily1/app/src/main/java/com/mobileapps/training/daily1/MainActivity.java
+++ Week4/Daily1/app/src/main/java/com/mobileapps/training/daily1/MainActivity.java
@@ -1,13 +1,50 @@
 package com.mobileapps.training.daily1;
 
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
+    CustomReceiver customReceiver;
+    IntentFilter intentFilter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: ");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        customReceiver = new CustomReceiver();
+        intentFilter = new IntentFilter();
+        intentFilter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
+        intentFilter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+        intentFilter.addAction(Intent.ACTION_CAMERA_BUTTON);
+        intentFilter.addAction(Intent.ACTION_SCREEN_OFF);
+        intentFilter.addAction(Intent.ACTION_HEADSET_PLUG);
+        this.registerReceiver(customReceiver,intentFilter);
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop: ");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy: ");
+        //Added the unregister here so the screen off intent will work
+        this.unregisterReceiver(customReceiver);
+        super.onDestroy();
     }
 }
